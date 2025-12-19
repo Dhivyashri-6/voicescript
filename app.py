89b1f14109188244f41c1165656a6163c6d8ed23
dhivyashri <dhivyashrikk.23it@kongu.edu>
2025-12-19 09:50:12 +0530

    Fix 404 errors: update static paths, fix CSS image url, add missing script, configure app host/port

--- app.py
+++ app.py
@@ -108,4 +108,5 @@ def download_file(filename):
     return send_from_directory(app.config['TRANSLATED_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
